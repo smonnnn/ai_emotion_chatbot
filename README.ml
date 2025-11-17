bfa1bf5e392b210dbb6bcbc584bc0245917cb87f
smonnnn <slooij1@gmail.com>
2025-11-17 13:51:06 +0100

    End sentences with a period.

--- README.ml
+++ README.ml
@@ -5,7 +5,7 @@ A simple chatbot that detects user emotions and generates empathetic responses.
 ## Setup
 
 1. If needed, modify setup.sh to install the right pytorch version for your machine.
-2. Run the setup script with `bash ./setup.sh`
-3. Set your mistral ai API key in the generated .env file
-4. Enable the generated venv with source. /.venv/bin/activate
+2. Run the setup script with `bash ./setup.sh`.
+3. Set your mistral ai API key in the generated .env file.
+4. Enable the generated venv with source. /.venv/bin/activate.
 5. Run `python main.py`.
