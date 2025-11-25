affbf1cd3fbc2bad21512063f4426a89d7330ae7
smonnnn <slooij1@gmail.com>
2025-11-25 17:04:08 +0100

    redoing the whole project because of that announcement saying I don't have to finetune and then suddenly requiring me to do it anyways

--- README.ml
+++ README.ml
@@ -1,11 +1,10 @@
-# Emotion-Adaptive Chatbot
+# Emotion-Finetuned Chatbot
 
-A simple chatbot that detects user emotions and generates empathetic responses.
+A simple chatbot that is finetuned to detect user emotions and generate empathetic responses.
 
 ## Setup
 
 1. If needed, modify setup.sh to install the right pytorch version for your machine.
 2. Run the setup script with `bash ./setup.sh`.
-3. Set your mistral ai API key in the generated .env file.
 4. Enable the generated venv with source. /.venv/bin/activate.
 5. Run `python main.py`.
--- setup.sh
+++ setup.sh
@@ -3,8 +3,4 @@ source ./.venv/bin/activate
 export TMPDIR="./temp"
 mkdir -p "./temp"
 pip3 install torch torchvision --index-url https://download.pytorch.org/whl/rocm6.4
-pip3 install openai dotenv transformers
-cat > .env << 'EOF'
-# Mistral AI API Key
-MISTRAL_API_KEY=sk-your-key-here
-EOF
+pip3 install transformers
